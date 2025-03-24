be534b9239d7b225df3ddcaf0d0845a46eb2363e
Jinpyo An <dkswlsvy3312@gmail.com>
2025-03-24 16:07:06 +0900

    섹션 7. 스프링 MVC - 기본 기능

---
+++ item-service/src/test/java/hello/itemservice/domain/item/ItemRepositoryTest.java
@@ -0,0 +1,61 @@
+package hello.itemservice.domain.item;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+class ItemRepositoryTest {
+
+    ItemRepository itemRepository = new ItemRepository();
+
+    @AfterEach
+    void afterEach() {
+        itemRepository.clearStore();
+    }
+
+
+    @Test
+    void save() {
+        Item item = new Item("itemA", 10000, 10);
+
+        final Item savedItem = itemRepository.save(item);
+
+        final Item findItem = itemRepository.findById(item.getId());
+        assertThat(findItem).isEqualTo(savedItem);
+    }
+
+    @Test
+    void findAll() {
+        Item item1 = new Item("item1", 10000, 10);
+        Item item2 = new Item("item2", 20000, 20);
+
+        itemRepository.save(item1);
+        itemRepository.save(item2);
+
+        final List<Item> result = itemRepository.findAll();
+
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result).contains(item1, item2);
+    }
+
+    @Test
+    void updateItem() {
+        final Item item = new Item("item1", 10000, 10);
+
+        final Item savedItem = itemRepository.save(item);
+        final Long itemId = savedItem.getId();
+
+        final Item updateParam = new Item("item2", 20000, 20);
+        itemRepository.update(itemId, updateParam);
+
+        final Item findItem = itemRepository.findById(itemId);
+
+        assertThat(findItem.getItemName()).isEqualTo(updateParam.getItemName());
+        assertThat(findItem.getPrice()).isEqualTo(updateParam.getPrice());
+        assertThat(findItem.getQuantity()).isEqualTo(updateParam.getQuantity());
+    }
+
+}
